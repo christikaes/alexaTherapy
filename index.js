1740b626790a85e2037a7b950adcc28cc6d8adb3
Johanna Bodnyk <bodnyk@gmail.com>
2016-01-23 18:15:43 -0500

    comments

--- index.js
+++ index.js
@@ -6,23 +6,18 @@ var indico = require("indico.io");
 indico.apiKey = '1bc005ab5acedb138d371dc703242b4a';
 
 var app = express();
-
 app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.json());
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'));
 });
 
-app.get('/hello', function(req, res) {
-  res.send('hello');
-});
-
-app.use(bodyParser.json());
-
+// Alexa therapy endpoint
+// Analyzes an utterance and responds with the speaker's mood
+// and a follow-up question
 app.post('/respond', function (req, res, next) {
     var issue = req.body.issue;
-    console.log(issue);
-
     analyzeIssue(issue, function(response) {
         res.json({
             "response" : response
@@ -38,6 +33,7 @@ function analyzeIssue(issue, callback) {
         speechOutput = "I didn't understand.";
     }
 
+    // Analyze utterance for positive or negative mood
     var getSentimentResponse =  function(){
         var onSentimentResponse = function(res) { 
             speechOutput = res > .5 ? "You sound happy." : "You sound sad.";
@@ -49,13 +45,16 @@ function analyzeIssue(issue, callback) {
             .catch(logError);
     }
 
+    // Analyze utterance for keywords and construct followup question
     var getKeywordsResponse = function(){
 
         var onKeywordResponse = function(keywords) {
+            // Tag keywords parts of speech          
             var tagger = new pos.Tagger();
             var taggedWords = tagger.tag(_.keys(keywords));
+            
+            // Find the most relevant keyword
             var topics = [];
-
             _.each(taggedWords, function(item) {
                 var word = item[0];
                 topics.push({
@@ -64,16 +63,20 @@ function analyzeIssue(issue, callback) {
                     confidence: keywords[word]
                 });
             });
+            // Nouns are probably the most relevant
             var nouns = _.filter(topics, function(topic) {
                 return topic.pos === "NN";
             });
+            // Which noun seemed the most important?
             nouns = _.sortBy(nouns, function(noun) {return noun.confidence;});
 
-            speechOutput += " Tell me more about the " + nouns[0].word;
+            // TODO: What if there isn't a noun? Add cases for adjectives and verbs.
 
+            speechOutput += " Tell me more about the " + nouns[0].word;
             callback(speechOutput);
         }
 
+        // Get keywords from utterance
         indico.keywords(issue, {version:2})
             .then(onKeywordResponse)
             .catch(logError);
@@ -81,12 +84,4 @@ function analyzeIssue(issue, callback) {
 
     getSentimentResponse();
 }
-
-app.post('/test', function(req, res) {
-    var issue = req.body.issue;
-    var response = {
-        "response" : issue
-    }
-  res.json(response);
-});
 
