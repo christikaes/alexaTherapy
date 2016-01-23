b157d1f01a920becbf880f7d22476ef68accf8ea
Christina <christikaes@gmail.com>
2016-01-23 16:14:30 -0500

    remove alexa test indico

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.set('view engine','ejs');
+// app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.set('view engine','ejs');
 
 
 // //create and assign our Alexa App instance to an address on express, in this case https://hey-dad.herokuapp.com/api/hey-dad
-var alexaApp = new alexa.app('hey-dad');
-alexaApp.express(app, "/api/");
+// var alexaApp = new alexa.app('hey-dad');
+// alexaApp.express(app, "/api/");
 
 //make sure we're listening on the assigned port
 app.listen(app.get('port'), function() {
@@ -34,14 +34,14 @@ var indico = require("indico.io");
 indico.apiKey = '1bc005ab5acedb138d371dc703242b4a';
 
 
-//our intent that is launched when "Hey Alexa, open Hey Dad" command is made
-//since our app only has the one function (tell a bad joke), we will just do that when it's launched
-alexaApp.launch(function(request,response) {
-    //log our app launch
-    console.log("App launched"); 
-    response.say("Welcome");
-    response.send();
-});
+// //our intent that is launched when "Hey Alexa, open Hey Dad" command is made
+// //since our app only has the one function (tell a bad joke), we will just do that when it's launched
+// alexaApp.launch(function(request,response) {
+//     //log our app launch
+//     console.log("App launched"); 
+//     response.say("Welcome");
+//     response.send();
+// });
 
 app.get('/hello', function(req, res) {
   res.send('hello');
