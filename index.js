55d05415e71dfc2f1e00dac41307cbc0fcf908d9
Johanna Bodnyk <bodnyk@gmail.com>
2016-01-23 17:43:24 -0500

    indico logic on heroku?

--- index.js
+++ index.js
@@ -1,55 +1,88 @@
-//initialize express
 var express = require('express');
-
-// //initialize alexa-app
-// var alexa = require('alexa-app');
-// var alexaApp = new alexa.app('alexa-therapy');
-
-// //initialize body-parser
 var bodyParser = require('body-parser');
-
-// //initialize the app and set the port
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-// app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-// app.set('view engine','ejs');
-
-
-// create and assign our Alexa App instance to an address on express, in this case https://hey-dad.herokuapp.com/api/hey-dad
-// var alexaApp = new alexa.app('alexa-therapy');
-// alexaApp.express(app, "/api/");
-
-//make sure we're listening on the assigned port
-app.listen(app.get('port'), function() {
-
-    console.log("Node app is running at localhost:" + app.get('port'));
-
-});
-
 var _ = require('underscore');
 var pos = require('pos');
 var indico = require("indico.io");
 indico.apiKey = '1bc005ab5acedb138d371dc703242b4a';
 
+var app = express();
 
-// //our intent that is launched when "Hey Alexa, open Hey Dad" command is made
-// //since our app only has the one function (tell a bad joke), we will just do that when it's launched
-// alexaApp.launch(function(request,response) {
-//     //log our app launch
-//     console.log("App launched"); 
-//     response.say("Welcome");
-//     response.send();
-// });
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
+});
 
 app.get('/hello', function(req, res) {
   res.send('hello');
 });
 
+app.use(bodyParser.json());
+
+app.post('/respond', function (req, res, next) {
+    var issue = req.body.issue;
+    console.log(issue);
+
+    analyzeIssue(issue, function(response) {
+        res.json({
+            "response" : response
+        });
+    });
+});
+
+function analyzeIssue(issue, callback) {
+    var speechOutput = "Uh oh it didn't work";
+
+    var logError = function(err) { 
+        console.log(err);
+        speechOutput = "I didn't understand.";
+    }
+
+    var getSentimentResponse =  function(){
+        var onSentimentResponse = function(res) { 
+            speechOutput = res > .5 ? "You sound happy." : "You sound sad.";
+            getKeywordsResponse();
+        }
+
+        indico.sentiment(issue)
+            .then(onSentimentResponse)
+            .catch(logError);
+    }
+
+    var getKeywordsResponse = function(){
+
+        var onKeywordResponse = function(keywords) {
+            var tagger = new pos.Tagger();
+            var taggedWords = tagger.tag(_.keys(keywords));
+            var topics = [];
+
+            _.each(taggedWords, function(item) {
+                var word = item[0];
+                topics.push({
+                    word: word,
+                    pos: item[1],
+                    confidence: keywords[word]
+                });
+            });
+            var nouns = _.filter(topics, function(topic) {
+                return topic.pos === "NN";
+            });
+            nouns = _.sortBy(nouns, function(noun) {return noun.confidence;});
+
+            speechOutput += " Tell me more about the " + nouns[0].word;
+
+            callback(speechOutput);
+        }
+
+        indico.keywords(issue, {version:2})
+            .then(onKeywordResponse)
+            .catch(logError);
+    }
+
+    getSentimentResponse();
+}
+
 app.post('/test', function(req, res) {
-    // console.log(req.body)
     var issue = req.body.issue;
     var response = {
         "response" : issue
@@ -57,21 +90,3 @@ app.post('/test', function(req, res) {
   res.json(response);
 });
 
-// alexa.intent('number',
-//   {
-//     "slots":{"number":"NUMBER"}
-//     ,"utterances":[ "say the number {1-100|number}" ]
-//   },
-//   function(request,response) {
-//     var number = request.slot('number');
-//     response.say("You asked for the number "+number);
-//   }
-// );
-
-// // Manually hook the handler function into express 
-// express.post('/alexa-therapy',function(req,res) {
-//   alexaApp.request(req.body)        // connect express to alexa-app 
-//     .then(function(response) { // alexa-app returns a promise with the response 
-//       res.json(response);      // stream it to express' output 
-//     });
-// });
