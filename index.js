5e8ec93ddd94a93d2084d92180616ad4c55a8eee
Christina <christikaes@gmail.com>
2016-01-23 17:17:08 -0500

    update post

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/test', function(req, res) {
     var response = {
         "response" : issue
     }
-  res.json(response);
+  res.json(JSON.stringify(response));
 });
 
 // alexa.intent('number',
